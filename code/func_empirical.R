ac61f540e375f6f7522341021c5bed7d51712a37
caitlinch <24237077+caitlinch@users.noreply.github.com>
2020-03-10 17:09:12 +1100

    Fix mismatch taxa issue - was getting .bionj not .treefile

--- code/func_empirical.R
+++ code/func_empirical.R
@@ -565,7 +565,7 @@ extract.num.informative.sites <- function(alignment_path){
 # Function to open an iqtree file and extract the number of parsimony informative sites
 extract.iqtree.tree <- function(alignment_path){
   # Open .iqtree file associated with that alignment
-  iqtree_filepath <- paste0(alignment_path,".bionj")
+  iqtree_filepath <- paste0(alignment_path,".treefile")
   if (file.exists(iqtree_filepath) == FALSE){
     return("MISSING_FILE")
   } else if (file.exists(iqtree_filepath) == TRUE){
