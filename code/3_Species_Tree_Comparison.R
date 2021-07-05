843838a3e0d9e890d94ffbc05018ac5f399d8acf
caitlinch <24237077+caitlinch@users.noreply.github.com>
2021-07-05 17:04:20 +1000

    Reformat ASTRAL trees for Julia

--- code/3_Species_Tree_Comparison.R
+++ code/3_Species_Tree_Comparison.R
@@ -8,7 +8,8 @@
 ##### Step 1: Set file paths and run variables #####
 run = "local"
 
-if (run == local){
+if (run == "local"){
+  # Datasets of interest
   input_names <- c("1KP", "Strassert2021","Vanderpool2020", "Pease2016")
   
   # File and directory locations
@@ -17,6 +18,62 @@ if (run == local){
                      "/Users/caitlincherryh/Documents/C1_EmpiricalTreelikeness/01_Data_Vanderpool2020/1730_Alignments_FINAL/",
                      "/Users/caitlincherryh/Documents/C1_EmpiricalTreelikeness/01_Data_Pease2016/all_window_alignments/")
   csv_data_dir <- "/Users/caitlincherryh/Documents/C1_EmpiricalTreelikeness/03_output/"
-  output_dir <- c("/Users/caitlincherryh/Documents/C1_EmpiricalTreelikeness/04_trees/")
+  tree_dir <- c("/Users/caitlincherryh/Documents/C1_EmpiricalTreelikeness/04_trees/")
+  output_dir <-c("/Users/caitlincherryh/Documents/C1_EmpiricalTreelikeness/05_dataAnalysis/")
   maindir <- "/Users/caitlincherryh/Documents/Repositories/empirical_treelikeness/" # where the empirical treelikeness code is
 }
+
+
+
+##### Step 2: Open packages and functions #####
+# Open packages
+library(ape)
+
+
+
+##### Step 3: Compare ASTRAL trees #####
+# For each test, I have a fail tree, a pass tree. I also have an all tree - one that includes every loci from the dataset
+# I want to take my three trees and modify the format to work for QGOF: add arbitrary terminal branches (1) and remove posterior probability values
+# Then I need to convert the list of gene trees to a list of gene trees that work for QGOF
+# Working in Julia:
+#   - Convert the list of gene trees into a set of quartet CFs
+#   - Apply the QGOF test and save the output values
+#   - Repeat this for the other two trees
+
+# Starting for one example:
+
+# Extract all the ASTRAL trees for one dataset
+dataset = "Vanderpool2020"
+dataset_folder <- paste0(output_dir, dataset, "/")
+if (dir.exists(dataset_folder) == FALSE){dir.create(dataset_folder)}
+# Extract the names of the ASTRAL species trees and the .txt files containing the list of gene trees
+species_tree_folder <- paste0(tree_dir, dataset, "/", "species_trees", "/")
+all_species_trees_files <- list.files(species_tree_folder)
+all_astral_trees <- grep("\\.tre", all_species_trees_files, value = TRUE)
+all_astral_gene_trees <- gsub("_species.tre", ".txt", all_astral_trees)
+all_astral_files <- c(all_astral_trees, all_astral_gene_trees)
+
+# For each test, create a new folder and copy the relevant files into that folder
+tests_to_run <- c("allTests", "PHI", "maxchi", "geneconv")
+t = "PHI"
+for (t in tests_to_run){
+  # Collect files for this test
+  files <- c(grep(t, all_astral_trees, value = TRUE), grep("NoTest", all_astral_trees, value = TRUE), 
+             grep("pass", grep(t, all_astral_gene_trees, value = TRUE), value = TRUE))
+  # Name the new folder for running this test
+  new_folder <- paste0(dataset_folder, "quarnetGoFtest_", t, "/")
+  if (dir.exists(new_folder) == FALSE){dir.create(new_folder)}
+  # Move files into this folder
+  for (i in files){
+    file.copy(from = paste0(species_tree_folder, i), to = paste0(new_folder, i))
+  }
+  
+  # Give files their new full filepath
+  files <- paste0(new_folder, files)
+  # Rewrite ASTRAL trees to match format for quarnetGoFtest
+  lapply(files[1:3], reformat.ASTRAL.tree.for.Julia)
+  
+}
+
+
+
--- code/func_comparison.R
+++ code/func_comparison.R
@@ -1,3 +1,20 @@
 ### empirical_treelikeness/code/func_comparison.R
 ## R functions for comparing two species trees
 # Caitlin Cherryh 2021
+
+# Function to read in tree from ASTRAL and edit it to use in the QuartetNetworkGoodnessOfFit julia package
+# ASTRAL does not output terminal branch lengths, so these are arbitrarily given the length 1
+# ASTRAL includes posterior probabilities: these are stripped (the Julia function cannot handle extra node values)
+reformat.ASTRAL.tree.for.Julia <- function(tree_path){
+  # Open tree
+  t <- read.tree(tree_path)
+  # Identify which edges correspond to terminal branches
+  n = length(t$tip.label)
+  missing_branch_indexes <- sapply(1:n,function(x,y)   which(y==x),y=t$edge[,2])
+  # Set length of terminal branches to 1
+  t$edge.length[missing_branch_indexes] <- 1
+  # Don't want node labels - for qaurnetGoFtest, need just taxa and branch lengths
+  t$node.label <- NULL
+  # Write this tree back out
+  write.tree(t, tree_path)
+}
