8d4b5658f9edc052d4508e697091fb0e05ac463c
caitlinch <24237077+caitlinch@users.noreply.github.com>
2021-07-16 12:43:45 +1000

    If quartet concordance factors have already been calculated, open that csv file

--- code/func_comparison.R
+++ code/func_comparison.R
@@ -77,11 +77,20 @@ write.Julia.GoF.script <- function(test_name, dataset, directory, pass_tree, fai
   # Add working directory
   lines <- c(lines, paste0('cd("', directory, '")'), '')
   # Add code for converting trees to quartet concordance factors
-  lines <- c(lines,
-             '# Convert list of gene trees to concordance factors - using trees with bootstraps works fine',
-             paste0('genetrees_cf = readTrees2CF("', gene_trees,
-                    '", writeTab=true, CFfile="', gene_cf_file, '")'), 
-             '')
+  if (file.exists(gene_cf_file == TRUE)){
+    # If the gene trees have already been converted into concordance factors, open the concordance factors csv file
+    lines <- c(lines,
+               '# Open quartet concordance factors file',
+               paste0('genetrees_cf = readTableCF("', gene_cf_file, '")'), 
+               '')
+  } else if (file.exists(gene_cf_file == FALSE)){
+    # If the quartet concordance factors have not been calculated, calculate them from the gene trees
+    lines <- c(lines,
+               '# Convert list of gene trees to concordance factors - using trees with bootstraps works fine',
+               paste0('genetrees_cf = readTrees2CF("', gene_trees,
+                      '", writeTab=true, CFfile="', gene_cf_file, '")'), 
+               '')
+  }
   # Open trees and root them 
   lines <- c(lines, 
              '# Read in the three trees',
