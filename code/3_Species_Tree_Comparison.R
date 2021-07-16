ad16176a6e67c5ebe87aec67c7b7e9000bcf6024
caitlinch <24237077+caitlinch@users.noreply.github.com>
2021-07-16 14:00:28 +1000

    Adding AU test

--- code/3_Species_Tree_Comparison.R
+++ code/3_Species_Tree_Comparison.R
@@ -1,7 +1,10 @@
 ### empirical_treelikeness/code/3_Species_Tree_Comparison.R
 ## R program to identify the best fitting tree for a candidate dataset
-## Additional software packages required:
-# 
+## Additional software/packages required:
+#     - IQ-Tree (http://www.iqtree.org/)
+#     - Julia (https://julialang.org/)
+#     - PhyloNetworks.jl (https://github.com/crsl4/PhyloNetworks.jl)
+#     - QuartetNetworkGoodnessFit.jl (https://github.com/cecileane/QuartetNetworkGoodnessFit.jl)
 # Caitlin Cherryh 2021
 
 
@@ -50,20 +53,22 @@ dataset_folder <- paste0(output_dir, dataset, "/")
 if (dir.exists(dataset_folder) == FALSE){dir.create(dataset_folder)}
 # Extract the names of the ASTRAL species trees and the .txt files containing the list of gene trees
 species_tree_folder <- paste0(tree_dir, dataset, "/", "species_trees", "/")
-all_species_trees_files <- list.files(species_tree_folder)
-all_astral_trees <- grep("\\.tre", all_species_trees_files, value = TRUE)
+all_species_trees_files <- list.files(species_tree_folder, recursive = TRUE)
+all_astral_files <- grep("ASTRAL", all_species_trees_files, value = TRUE)
+all_astral_trees <- grep("\\.tre", all_astral_files, value = TRUE)
 all_astral_gene_trees <- gsub("_species.tre", ".txt", all_astral_trees)
-all_astral_files <- c(all_astral_trees, all_astral_gene_trees)
+all_IQTree_files <- grep("IQTREE", all_species_trees_files, value = TRUE)
+all_IQTree_partitions <- grep(".nex.", grep("partitions.nex", all_IQTree_files, value = TRUE), value = TRUE, invert = TRUE)
+all_IQTree_trees <- gsub(".nex", ".nex.contree", all_IQTree_partitions)
 
 # For each test, create a new folder and copy the relevant files into that folder
 tests_to_run <- c("allTests", "PHI", "maxchi", "geneconv")
 test = "PHI"
 for (test in tests_to_run){
+  ## Apply the Quartet Network Goodness of Fit test in Julia
   # Name the new folder for running this test
   new_folder <- paste0(dataset_folder, "quarnetGoFtest_", test, "/")
   if (dir.exists(new_folder) == FALSE){dir.create(new_folder)}
-  
-  # Check whether the quartet CFs have been estimated for the NoTest gene trees file and if not, convert them
   
   # Check whether the results file for this test exists already
   quarnet_results_file <- paste0(new_folder, dataset, "_", test, "_QuarNetGoF_test_results.csv")
@@ -91,6 +96,39 @@ for (test in tests_to_run){
     # Write the Julia code into a file
     write.Julia.GoF.script(test_name = test, dataset = dataset, directory = new_folder, pass_tree = pass_tree_file, fail_tree = fail_tree_file, 
                            all_tree = noTest_tree_file, gene_trees = gene_trees_file, tree_root = tree_root, output_csv_file_path = quarnet_results_file)
+    # Run the script in Julia to calculate the adequacy of each tree for the quartet concordance factors calculated from the gene trees
+    
+    
+    ## Apply the AU tests in IQ-Tree
+    # Name the new folder for running this test
+    new_folder <- paste0(dataset_folder, "AUtest_", test, "/")
+    if (dir.exists(new_folder) == FALSE){dir.create(new_folder)}
+    
+    # Check whether the results file for this test exists already
+    au_results_file <- paste0(new_folder, dataset, "_", test, "_AU_test_results.csv")
+    # If the file does not exist, run the tests
+    if (file.exists(au_results_file) == FALSE){
+      # Collect files for this test: three trees and the partition file (containing the loci that pass the test)
+      test_IQTREE_trees <- grep(test, all_IQTree_trees, value = TRUE)
+      # Sort the files into the following order: test pass, test fail, no test
+      three_trees_location <- c(grep("pass", test_IQTREE_trees, value = TRUE), grep("fail", test_IQTREE_trees, value = TRUE), grep("NoTest", all_IQTree_trees, value = TRUE))
+      three_trees_location <- paste0(species_tree_folder, three_trees_location)
+      # Read in the three trees
+      three_trees_text <- unlist(lapply(three_trees_location, readLines))
+      # Write the three trees into one file, inside the new folder for the AU test
+      three_trees_path <- paste0(new_folder, dataset, "_", test, "three_trees_Pass-Fail-NoTest.tree")
+      write(three_trees_text, three_trees_path)
+      # Find the partitions file containing the location of the loci that pass the test
+      test_pass_partition_file <- grep("pass", grep(test, all_IQTree_partitions, value = TRUE), value = TRUE)
+      # Write the new name for the partition file
+      partition_path <- paste0(new_folder, "partitions.nex")
+      # Copy the partition file
+      file.copy(from = paste0(species_tree_folder, test_pass_partition_file), to = partition_path)
+      
+      # Apply the AU test
+      
+    }
+
   }
   
 }
