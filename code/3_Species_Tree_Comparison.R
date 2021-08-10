5af2b69692f22f084605a4b85bacec7344a68442
caitlinch <24237077+caitlinch@users.noreply.github.com>
2021-08-10 18:03:13 +1000

    Do not add arbitrary terminal branches for QuartetNetwork GoF.jl analysis - but still remove node labels

--- code/3_Species_Tree_Comparison.R
+++ code/3_Species_Tree_Comparison.R
@@ -135,9 +135,9 @@ if (run_Julia_QuarNetGoF_test == TRUE){
         files <- paste0(new_folder, files)
         
         # Rewrite ASTRAL trees to match format for quarnetGoFtest (will all have .tre extension)
-        lapply(grep(".tre", files, value = TRUE), reformat.ASTRAL.tree.for.Julia)
+        lapply(grep(".tre", files, value = TRUE), reformat.ASTRAL.tree.for.Julia, add.arbitrary.terminal.branches = FALSE)
         # Rewrite ASTRAL gene trees to match format for quarnetGoFTest (will have .txt extension)
-        lapply(grep(".txt", files, value = TRUE), reformat.gene.tree.list.for.Julia, gene.tree.source = "IQ-TREE")
+        lapply(grep(".txt", files, value = TRUE), reformat.gene.tree.list.for.Julia, add.arbitrary.terminal.branches = FALSE)
         # Name the files
         noTest_tree_file <- grep("NoTest", grep("tre", files, value = TRUE), value = TRUE)
         pass_tree_file <- grep("pass", grep("tre", files, value = TRUE), value = TRUE)
--- code/func_comparison.R
+++ code/func_comparison.R
@@ -5,15 +5,17 @@
 # Function to read in tree from ASTRAL and edit it to use in the QuartetNetworkGoodnessOfFit Julia package
 # ASTRAL does not output terminal branch lengths, so these are arbitrarily given the length 1
 # ASTRAL includes posterior probabilities: these are stripped (the Julia function cannot handle extra node values)
-reformat.ASTRAL.tree.for.Julia <- function(tree_path){
+reformat.ASTRAL.tree.for.Julia <- function(tree_path, add.arbitrary.terminal.branches = FALSE){
   # Open tree
   t <- read.tree(tree_path)
-  # Identify which edges correspond to terminal branches
-  n = length(t$tip.label)
-  missing_branch_indexes <- sapply(1:n,function(x,y)   which(y==x),y=t$edge[,2])
-  # Set length of terminal branches to 1
-  t$edge.length[missing_branch_indexes] <- 1
-  # Don't want node labels - for qaurnetGoFtest, need just taxa and branch lengths
+  if (add.arbitrary.terminal.branches == TRUE){
+    # Identify which edges correspond to terminal branches
+    n = length(t$tip.label)
+    missing_branch_indexes <- sapply(1:n,function(x,y)   which(y==x),y=t$edge[,2])
+    # Set length of terminal branches to 1
+    t$edge.length[missing_branch_indexes] <- 1
+  }
+  # Don't want node labels - for quartnetGoFtest, need just taxa and branch lengths
   t$node.label <- NULL
   # Write this tree back out
   write.tree(t, tree_path)
@@ -24,12 +26,12 @@ reformat.ASTRAL.tree.for.Julia <- function(tree_path){
 # Function to read in list of genetrees from ASTRAL and edit to use in the QuartetNetworkGoodnessOfFit Julia package
 # ASTRAL does not output terminal branch lengths, so these are arbitrarily given the length 1
 # ASTRAL includes posterior probabilities: these are stripped (the Julia function cannot handle extra node values)
-reformat.gene.tree.list.for.Julia <- function(trees_path, gene.tree.source = "IQ-TREE"){
+reformat.gene.tree.list.for.Julia <- function(trees_path, add.arbitrary.terminal.branches = FALSE){
   # Open trees
   ts <- read.tree(trees_path)
   for (i in 1:length(ts)){
     temp_tree <- ts[[i]]
-    temp_tree <- reformat.phylo.for.Julia(temp_tree, gene.tree.source)
+    temp_tree <- reformat.phylo.for.Julia(temp_tree, add.arbitrary.terminal.branches)
     ts[[i]] <- temp_tree
   }
   # Write these trees back out
@@ -37,9 +39,9 @@ reformat.gene.tree.list.for.Julia <- function(trees_path, gene.tree.source = "IQ
 }
 
 # Function to take one tree from a multiphylo object, format it for QuartetNetworkGoodnessOfFit Julia package, and return it
-reformat.phylo.for.Julia <- function(tree, gene.tree.source = "IQ-TREE"){
+reformat.phylo.for.Julia <- function(tree, add.arbitrary.terminal.branches = FALSE){
   # If gene trees from ASTRAL, the terminal branches are not output and need to be set to one
-  if (gene.tree.source == "ASTRAL"){
+  if (add.arbitrary.terminal.branches == TRUE){
     # Identify which edges correspond to terminal branches
     n = length(tree$tip.label)
     missing_branch_indexes <- sapply(1:n,function(x,y)   which(y==x),y=tree$edge[,2])
@@ -112,7 +114,7 @@ write.Julia.GoF.script <- function(test_name, dataset, directory, pass_tree, fai
              paste0("gof_test_fail = quarnetGoFtest!(tree_test_fail, genetrees_cf, false; quartetstat=:LRT, correction=:simulation, seed=", seed,
                     ", nsim=", number_of_simulated_replicates, ", verbose=false, keepfiles=false)"),
              paste0("gof_test_all = quarnetGoFtest!(tree_all, genetrees_cf, false; quartetstat=:LRT, correction=:simulation, seed=", seed ,
-                   ", nsim=", number_of_simulated_replicates, ", verbose=false, keepfiles=false)"),
+                    ", nsim=", number_of_simulated_replicates, ", verbose=false, keepfiles=false)"),
              "")
   # Create an output dataframe
   lines <- c(lines,
