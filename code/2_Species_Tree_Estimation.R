ebc0f5ea65da23c5454e51bab2e7667084cd2c4b
caitlinch <24237077+caitlinch@users.noreply.github.com>
2022-05-09 14:22:01 +1000

    Remove Meteazoan GENECONV fail tree (not enough loci)

--- code/2_Species_Tree_Estimation.R
+++ code/2_Species_Tree_Estimation.R
@@ -154,7 +154,7 @@ if (run_location == "local"){
                                "Pease2016" = c("PHI", "maxchi", "geneconv", "all"))
   # List of trees to estimate for each dataset
   dataset_trees_to_estimate <- list("1KP" = c("PHI,pass", "maxchi,pass"),
-                                    "Whelan2017" = c("PHI", "maxchi", "geneconv,pass", "geneconv,fail"),
+                                    "Whelan2017" = c("PHI", "maxchi", "geneconv,pass"),
                                     "Vanderpool2020" = c("PHI,pass", "PHI,fail", "maxchi,pass", "maxchi,fail",
                                                          "geneconv,pass", "geneconv,fail", "all,pass", "all,fail"),
                                     "Pease2016" = c("PHI,pass", "PHI,fail", "maxchi,pass", "maxchi,fail",
