d96f84a02cbee0c87ec798cbf497d46e569c82b6
caitlinch <24237077+caitlinch@users.noreply.github.com>
2023-06-21 15:50:21 +1000

    New script to plot tanglegrams of species trees.

---
+++ code/5_Tanglegrams.R
@@ -0,0 +1,69 @@
+### gene_filtering/code/5_Tanglegrams.R
+## R program to plot tanglegrams from the species trees
+# Caitlin Cherryh 2023
+
+## This script creates a variety of plots and figures from the subset trees and species trees for the 4 empirical datasets
+
+## ggtree notes:
+# To add node labels (for defining clade labels): geom_text(aes(label=node), hjust=-.3)
+# To add clade labels (for defining clades with a vertical bar): geom_cladelab(node=34, label="Clade")
+
+
+
+##### Step 1: Set the file paths for input and output files, and necessary functions/directories #####
+# maindir                 <- "gene_filtering" repository location (github.com/caitlinch/gene_filtering)
+# plot_dir                <- for saving plots and analyses.
+# annotations_csv_file    <- location of misc/annotations.csv file from the Leebens-Mack (2019) "Data from 1000 Plants Transcriptomes" data repository
+# roots_by_groups         <- set which taxa is outgroup for each dataset: Primates, Tomatoes, Metazoans, and Plants
+
+### Caitlin's paths ###
+# Folders and filepaths
+maindir <- "/Users/caitlincherryh/Documents/Repositories/gene_filtering/"
+plot_dir <- "/Users/caitlincherryh/Documents/C1_EmpiricalTreelikeness/06_results/plots/"
+annotation_csv_file <- "/Users/caitlincherryh/Documents/C1_EmpiricalTreelikeness/01_Data_1KP/misc/annotations.csv"
+
+# Dataset information
+roots_by_group <- list("Plants" = c("BAKF", "ROZZ", "MJMQ", "IRZA", "IAYV", "BAJW", "APTP", "LXRN", "NMAK", "RFAD", "LLEN", "RAPY", "OGZM",
+                                    "QDTV", "FIDQ", "EBWI", "JQFK", "BOGT", "VKVG", "DBYD", "FSQE", "LIRF", "QLMZ", "JCXF", "ASZK", "ULXR",
+                                    "VRGZ", "LDRY", "VYER", "FIKG", "RWXW", "FOMH", "YRMA", "HFIK", "JGGD"), 
+                       "Metazoan" = c("Salpingoeca_pyxidium", "Monosiga_ovata", "Acanthoeca_sp", "Salpingoeca_rosetta", "Monosiga_brevicolis"), 
+                       "Primates" = c("Mus_musculus"), 
+                       "Tomatoes" = c("LA4116", "LA2951", "LA4126"))
+### End of Caitlin's paths ###
+
+
+
+#### Step 2: Open files and packages ####
+# Open packages
+library(ape) # functions: read.tree, Ntip, root
+library(ggplot2) # for nice plots
+library(ggtree) # for plotting phylogenetic trees
+library(ggtext) # for nice tree plots
+library(patchwork) # for collating plots
+# library(phangorn) # for densiTree function - replaced by ggdensitree
+library(TreeTools) # for CollapseNode function
+
+# Source functions
+source(paste0(maindir,"code/func_plots.R"))
+
+# Assemble folder for species trees
+species_tree_folder <- paste0(maindir, "species_trees/")
+
+
+
+#### Step 3: Plotting Primates dataset ####
+
+
+
+#### Step 4: Plotting Tomatoes dataset ####
+
+
+
+#### Step 5: Plotting Metazoans dataset ####
+
+
+
+#### Step 6: Plotting Plants dataset ####
+
+
+
